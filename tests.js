52bf2afbe3ca906be63b9a4ed820af02c101e49d
jwhester <jameshester88@gmail.com>
2019-10-11 15:20:44 -0500

    3rd exercise

--- tests.js
+++ tests.js
@@ -21,4 +21,7 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe("string");
     });
+    it('should return the string "Hello, Alex!" when executed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!")
+    })
 });
