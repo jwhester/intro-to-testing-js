35c0878cf8cdb6b2ad047be5c12a5728f4cf66af
jwhester <jameshester88@gmail.com>
2019-10-11 16:07:19 -0500

    test for edge cases

--- code.js
+++ code.js
@@ -2,6 +2,11 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
 function sayHello(input) {
-    return "Hello, " + input + "!";
+    if (input === true || input === false || input === "" || input === null) {
+        return helloWorld();
+    } else {
+        return "Hello, " + input + "!";
+    }
 }
