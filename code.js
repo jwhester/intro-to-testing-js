c0b770a65693da764004a83981024269afdbb819
jwhester <jameshester88@gmail.com>
2019-10-11 15:35:29 -0500

    5th exercise

--- code.js
+++ code.js
@@ -5,7 +5,9 @@ function helloWorld() {
 function sayHello(input) {
     if (input === "Alex"){
         return "Hello, Alex!";
+    } else if(input === "Pat"){
+        return "Hello, Pat!";
     } else {
-        return "Hello, Jane!";
+        return "Hello, Jane!"
     }
 }
